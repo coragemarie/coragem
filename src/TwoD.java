c383dc6ced3122ba2cda23d81e6d623ee7bdbc2f
Student <Student@CLB3Unit35>
2024-01-24 09:12:37 +0800

    corage

---
+++ src/TwoD.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class TwoD {
+    
+    public static void main(string[] args) {
+        
+        int []oneDArray = {55,5,1,3};
+        int [][] twoDArray = {
+            /*0*/  {1,2,3},
+            /*1*/  {4,5,6},
+           /*2*/   {7,8,9},   
+        };
+        
+       int xRow = 1;
+       int yColumn = 2;
+       
+        System.out.println("Your array is; " + twoDArray [xRow][yColumn]);
+    }
+    
+}
